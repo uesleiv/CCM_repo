49e3231085c971033d27929119aab120ecb28629
Ueslei <snypes@gmail.com>
2016-01-07 23:43:45 -0200

    membros

--- resources/views/membro/index.blade.php
+++ resources/views/membro/index.blade.php
@@ -6,21 +6,34 @@
 </head>
 <body>
 <ul>
-    <?php if($membros->count()) : ?>
-        <?php foreach ($membros as $membro): ?>
-        <li>
-            Id: <?php echo $membro->id; ?>
-            <br/>
-            Nome: <?php echo $membro->nome; ?>
-            <br/>
-            Sexo: <?php echo $membro->sexo; ?>
-            <br/>
-            Email: <?php echo $membro->email; ?>
-        </li>
-        <?php endforeach; ?>
-    <?php else : ?>
+    @if($membros->count())
+        @foreach($membros as $membro)
+            <li>
+                Id: {{{ $membro->id }}}</br>
+                Nome: {{{ $membro->nome }}} </br>
+                Sexo: {{{ $membro->sexo }}} </br>
+                Email: {{{ $membro->email }}}</br>
+            </li>
+        @endforeach
+    @else
         <h2>Nenhum membro encontrado.</h2>
-    <?php endif ?>
+    @endif
+    
+    <!-- <?//php if($membros->count()) : ?>
+        <?//php foreach ($membros as $membro): ?>
+        <li>
+            Id: <?//php echo $membro->id; ?>
+            <br/>
+            Nome: <?//php echo $membro->nome; ?>
+            <br/>
+            Sexo: <?//php echo $membro->sexo; ?>
+            <br/>
+            Email: <?//php echo $membro->email; ?>
+        </li>
+        <?//php endforeach; ?>
+    <?//php else : ?>
+        <h2>Nenhum membro encontrado.</h2>
+    <?//php endif ?> -->
 </ul>
 <a href="{!!URL::route('membro.create')!!}">Cadastrar</a>
 </body>
