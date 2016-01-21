6b8e2ac3c108a78eac8115d9d4d6704ff69569a1
Ueslei <snypes@gmail.com>
2016-01-20 23:59:58 -0200

    membros

--- resources/views/admin_template.blade.php
+++ resources/views/admin_template.blade.php
@@ -11,18 +11,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
-    <link rel="stylesheet" href="{{asset("vendor/almasaeed2010/adminlte/bootstrap/css/bootstrap.min.css")}}">
+    <link rel="stylesheet" href="{{asset("/adminlte/bootstrap/css/bootstrap.min.css")}}">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset("vendor/almasaeed2010/adminlte/dist/css/AdminLTE.min.css")}}">
+    <link rel="stylesheet" href="{{asset("/adminlte/dist/css/AdminLTE.min.css")}}">
     <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
           page. However, you can choose any other skin. Make sure you
           apply the skin class to the body tag so the changes take effect.
     -->
-    <link rel="stylesheet" href="{{asset("vendor/almasaeed2010/adminlte/dist/css/skins/skin-blue.min.css")}}">
+    <link rel="stylesheet" href="{{asset("/adminlte/dist/css/skins/skin-blue.min.css")}}">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -90,7 +90,7 @@ desired effect
                                         <a href="#">
                                             <div class="pull-left">
                                                 <!-- User Image -->
-                                                <img src="{{asset("/vendor/almasaeed2010/adminlte/dist/img/user2-160x160.jpg")}}" class="img-circle" alt="User Image">
+                                                <img src="{{asset("/adminlte/dist/img/user2-160x160.jpg")}}" class="img-circle" alt="User Image">
                                             </div>
                                             <!-- Message title and timestamp -->
                                             <h4>
@@ -174,14 +174,14 @@ desired effect
                         <!-- Menu Toggle Button -->
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <!-- The user image in the navbar-->
-                            <img src="{{asset("/vendor/almasaeed2010/adminlte/dist/img/user2-160x160.jpg")}}" class="user-image" alt="User Image">
+                            <img src="{{asset("/adminlte/dist/img/user2-160x160.jpg")}}" class="user-image" alt="User Image">
                             <!-- hidden-xs hides the username on small devices so only the image appears. -->
                             <span class="hidden-xs">Alexander Pierce</span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- The user image in the menu -->
                             <li class="user-header">
-                                <img src="{{asset("/vendor/almasaeed2010/adminlte/dist/img/user2-160x160.jpg")}}" class="img-circle" alt="User Image">
+                                <img src="{{asset("/adminlte/dist/img/user2-160x160.jpg")}}" class="img-circle" alt="User Image">
 
                                 <p>
                                     Alexander Pierce - Web Developer
@@ -231,7 +231,7 @@ desired effect
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel">
                 <div class="pull-left image">
-                    <img src="{{asset("/vendor/almasaeed2010/adminlte/dist/img/user2-160x160.jpg")}}" class="img-circle" alt="User Image">
+                    <img src="{{asset("/adminlte/dist/img/user2-160x160.jpg")}}" class="img-circle" alt="User Image">
                 </div>
                 <div class="pull-left info">
                     <p>Alexander Pierce</p>
@@ -385,11 +385,11 @@ desired effect
 <!-- REQUIRED JS SCRIPTS -->
 
 <!-- jQuery 2.1.4 -->
-<script src="{{asset("/vendor/almasaeed2010/adminlte/plugins/jQuery/jQuery-2.1.4.min.js")}}"></script>
+<script src="{{asset("/adminlte/plugins/jQuery/jQuery-2.1.4.min.js")}}"></script>
 <!-- Bootstrap 3.3.5 -->
-<script src="{{asset("/vendor/almasaeed2010/adminlte/bootstrap/js/bootstrap.min.js")}}"></script>
+<script src="{{asset("/adminlte/bootstrap/js/bootstrap.min.js")}}"></script>
 <!-- AdminLTE App -->
-<script src="{{asset("/vendor/almasaeed2010/adminlte/dist/js/app.min.js")}}"></script>
+<script src="{{asset("/adminlte/dist/js/app.min.js")}}"></script>
 
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
      Both of these plugins are recommended to enhance the
