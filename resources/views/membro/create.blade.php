f7ad21d36a0f3e4f458df50af05442421d74de5b
Ueslei <snypes@gmail.com>
2016-01-25 22:51:47 -0200

    membros

--- resources/views/membro/create.blade.php
+++ resources/views/membro/create.blade.php
@@ -7,6 +7,24 @@
         <label for="email">Email:</label>
         <input type="email" name="email" placeholder="Informe o email" required>
         <label for="sexo">Sexo:</label>
+        <select name="sexo">
+            <option value="M">M</option>
+            <option value="F">F</option>
+        </select>
+
+        <label for="rg">RG</label>
+        <input type="text" name="rg">
+
+        <label for="cpf">CPF</label>
+        <input type="text" name="cpf" placeholder="   .   .  -  ">
+
+        <div class="input-group">
+            <div class="input-group-addon">
+                <i class="fa fa-calendar"></i>
+            </div>
+            <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask>
+        </div>
+
 
 
         <div class="input-group">
@@ -15,12 +33,9 @@
             </div>
             <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask>
         </div>
-        <!-- /.input group -->
 
-        <select name="sexo">
-            <option value="M">M</option>
-            <option value="F">F</option>
-        </select>
+
+
         <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
         <input type="submit">
     </form>
