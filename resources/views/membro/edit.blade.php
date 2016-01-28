52948ab7fa90dddffd55148637fcce28f845f525
Ueslei <snypes@gmail.com>
2016-01-27 23:40:59 -0200

    membros

--- resources/views/membro/edit.blade.php
+++ resources/views/membro/edit.blade.php
@@ -8,9 +8,9 @@
             <label for="email">Email:</label>
             <input type="email" name="email" placeholder="Informe o email" required value="{{{ $membro->email}}}">
             <label for="sexo">Sexo:</label>
-            <<select name="sexo">
-                <option value="M" {{$membro->sexo = "M" ? "selected" : ""}}>Masculino</option>
-                <option value="F" {{$membro->sexo = "F" ? "selected" : ""}}>Feminino</option>
+            <select name="sexo">
+                <option value="M" {{$membro->sexo == "M" ? "selected" : ""}}>Masculino</option>
+                <option value="F" {{$membro->sexo == "F" ? "selected" : ""}}>Feminino</option>
             </select>
             <label for="rg">RG</label>
             <input type="text" name="rg">
