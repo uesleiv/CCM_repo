7ae6f1d0e7403b9b27ea26792ce7c2f230fb3c27
Ueslei <snypes@gmail.com>
2016-01-27 23:11:35 -0200

    membros

--- resources/views/membro/edit.blade.php
+++ resources/views/membro/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('admin_template')
 
 @section('content')
-    <form action="store" method="post">
+    <form action="{!!URL::route('membro.store')!!}" method="post">
         @if($membro->count())
             <label for="nome">Nome:</label>
             <input type="text" name="nome" placeholder="Informe o nome" required value="{{{ $membro->nome }}}">
